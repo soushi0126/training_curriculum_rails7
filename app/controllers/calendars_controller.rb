a9d7c7937995d12fcb2b407079f219537a9b3a94
soushi0126 <sousi0126@gmail.com>
2023-11-23 21:48:05 +0900

    ハッシュロケットの修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -14,7 +14,7 @@ class CalendarsController < ApplicationController
 
   private
 
-  
+
   def plan_params
     params.require(:plan).permit(:date, :plan)
   end
@@ -40,7 +40,7 @@ class CalendarsController < ApplicationController
         wday_num = wday_num - 7
       end
 
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => wdays[(@todays_date + x).wday]}
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[(@todays_date + x).wday]}
 
       @week_days.push(days)
     end
